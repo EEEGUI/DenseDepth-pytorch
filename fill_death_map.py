f4017e7f2c37190c323ce2daca1ae0a4011ce8a7
lin <1462243304@qq.com>
2019-03-07 21:09:03 +0800

    update code

--- fill_death_map.py
+++ fill_death_map.py
@@ -15,6 +15,136 @@ def img2depth(filename):
     # depth[depth_png == 0] = -1.
     return depth
 
+
 def depth2img(depth, filename):
     depth = (depth * 256).astype('int16')
-    depth
+    depth_png = Image.fromarray(depth)
+    depth_png.save(filename)
+
+
+def fill_depth_colorization(rgb_filename, depth_filename, alpha=1):
+    imgRgb = np.array(Image.open(rgb_filename), dtype=int)
+    imgRgb = imgRgb / np.max(imgRgb)
+    imgDepthInput = img2depth(depth_filename)
+
+    imgIsNoise = imgDepthInput == 0
+    maxImgAbsDepth = np.max(imgDepthInput)
+    imgDepth = imgDepthInput / maxImgAbsDepth
+    imgDepth[imgDepth > 1] = 1
+    (H, W) = imgDepth.shape
+    numPix = H * W
+    indsM = np.arange(numPix).reshape((W, H)).transpose()
+    knownValMask = (imgIsNoise == False).astype(int)
+    grayImg = skimage.color.rgb2gray(imgRgb)
+    winRad = 1
+    len_ = 0
+    absImgNdx = 0
+    len_window = (2 * winRad + 1) ** 2
+    len_zeros = numPix * len_window
+    cols = np.zeros(len_zeros) - 1
+    rows = np.zeros(len_zeros) - 1
+    vals = np.zeros(len_zeros) - 1
+    gvals = np.zeros(len_window) - 1
+
+    for j in range(W):
+        for i in range(H):
+            nWin = 0
+            for ii in range(max(0, i - winRad), min(i + winRad + 1, H)):
+                for jj in range(max(0, j - winRad), min(j + winRad + 1, W)):
+                    if ii == i and jj == j:
+                        continue
+
+                    rows[len_] = absImgNdx
+                    cols[len_] = indsM[ii, jj]
+                    gvals[nWin] = grayImg[ii, jj]
+
+                    len_ = len_ + 1
+                    nWin = nWin + 1
+
+            curVal = grayImg[i, j]
+            gvals[nWin] = curVal
+            c_var = np.mean((gvals[:nWin + 1] - np.mean(gvals[:nWin + 1])) ** 2)
+
+            csig = c_var * 0.6
+            mgv = np.min((gvals[:nWin] - curVal) ** 2)
+            if csig < -mgv / np.log(0.01):
+                csig = -mgv / np.log(0.01)
+
+            if csig < 2e-06:
+                csig = 2e-06
+
+            gvals[:nWin] = np.exp(-(gvals[:nWin] - curVal) ** 2 / csig)
+            gvals[:nWin] = gvals[:nWin] / sum(gvals[:nWin])
+            vals[len_ - nWin:len_] = -gvals[:nWin]
+
+            # Now the self-reference (along the diagonal).
+            rows[len_] = absImgNdx
+            cols[len_] = absImgNdx
+            vals[len_] = 1  # sum(gvals(1:nWin))
+
+            len_ = len_ + 1
+            absImgNdx = absImgNdx + 1
+
+    vals = vals[:len_]
+    cols = cols[:len_]
+    rows = rows[:len_]
+    A = scipy.sparse.csr_matrix((vals, (rows, cols)), (numPix, numPix))
+
+    rows = np.arange(0, numPix)
+    cols = np.arange(0, numPix)
+    vals = (knownValMask * alpha).transpose().reshape(numPix)
+    G = scipy.sparse.csr_matrix((vals, (rows, cols)), (numPix, numPix))
+
+    A = A + G
+    b = np.multiply(vals.reshape(numPix), imgDepth.flatten('F'))
+
+    # print ('Solving system..')
+
+    new_vals = spsolve(A, b)
+    new_vals = np.reshape(new_vals, (H, W), 'F')
+
+    # print ('Done.')
+
+    denoisedDepthImg = new_vals * maxImgAbsDepth
+
+    output = denoisedDepthImg.reshape((H, W)).astype('float32')
+
+    output = np.multiply(output, (1 - knownValMask)) + imgDepthInput
+
+    return output
+
+
+def main():
+    root = '~/Documents/DataSet/KITTI/'
+
+    if not os.path.exists(root + 'depth_maps_filled'):
+        os.mkdir(root + 'depth_maps_filled')
+
+    depth_folds = os.listdir(root + 'data_depth_annotated')
+    rgb_folds = os.listdir(root + 'KITTI_raw_data')
+
+    for fold in depth_folds:
+        if fold in rgb_folds:
+            if not os.path.exists(root + 'depth_maps_filled/' + fold):
+                os.mkdir(root + 'depth_maps_filled/' + fold)
+
+            depth_img_root = '%sdata_depth_annotated/train/%s/proj_depth/groundtruth/image_02/' % (root, fold)
+            rgb_img_root = '%sKITTI_raw_data/%s/image_02/data/' % (root, fold)
+            depth_filled_img_root = '%sdepth_maps_filled/%s/' % (root, fold)
+
+            depth_images = os.listdir(depth_img_root)
+            rgb_images = os.listdir(rgb_img_root)
+            depth_filled_images = os.listdir(depth_filled_img_root)
+            for img_name in depth_images:
+                assert (img_name in rgb_images)
+                if img_name not in depth_filled_images:
+                    depth_filename = depth_img_root + img_name
+                    rgb_filename = rgb_img_root + img_name
+                    depth_filled_filename = depth_filled_img_root + img_name
+                    depth = fill_depth_colorization(rgb_filename, depth_filename)
+                    depth2img(depth, depth_filled_filename)
+
+
+
+if __name__ == '__main__':
+    main()
--- fill_nan.py
+++ fill_nan.py
@@ -14,7 +14,8 @@ from PIL import Image
 from read_depth import depth_read
 import time
 import numba as nb
-
+import pandas as pd
+from multiprocessing.dummy import Pool as ThreadPool
 #
 # fill_depth_colorization.m
 # Preprocesses the kinect depth image using a gray scale version of the
@@ -166,8 +167,6 @@ def fill_depth_colorization(imgRgb=None, imgDepthInput=None, alpha=1):
     return output
 
 
-
-
 def fill_depth_colorization_jit(imgRgb=None, imgDepthInput=None, alpha=1):
     t0 = time.time()
     imgIsNoise = imgDepthInput == 0
@@ -261,13 +260,109 @@ def fill_depth_colorization_jit(imgRgb=None, imgDepthInput=None, alpha=1):
     print('t3=%.2f' % (t3-t2))
     return output
 
+def fill_depth_colorization_fpf(imgRgb=None, imgDepthInput=None, alpha=1):
+    t0 = time.time()
+    imgIsNoise = imgDepthInput == 0
+    maxImgAbsDepth = np.max(imgDepthInput)
+    imgDepth = imgDepthInput / maxImgAbsDepth
+    imgDepth[imgDepth > 1] = 1
+    (H, W) = imgDepth.shape
+    numPix = H * W
+    indsM = np.arange(numPix)
+    indsM_copy = np.arange(numPix).reshape((W, H)).transpose()
+    knownValMask = (imgIsNoise == False).astype(int)
+    grayImg = skimage.color.rgb2gray(imgRgb)
+    winRad = 1
+    len_window = (2 * winRad + 1) ** 2
+    len_zeros = numPix * len_window
+
+    cols = []
+    rows = []
+    vals = []
+    t1 = time.time()
+    print('t1=%.2f' % (t1-t0))
+    def func(x):
+
+        i = x % H
+        j = x // H
+        nWin = 0
+        gvals = np.zeros(len_window) - 1
+        for ii in range(max(0, i - winRad), min(i + winRad + 1, H)):
+            for jj in range(max(0, j - winRad), min(j + winRad + 1, W)):
+                if ii == i and jj == j:
+                    continue
+
+                rows.append(x)
+                cols.append(indsM_copy[ii][jj])
+                gvals[nWin] = grayImg[ii, jj]
+                nWin = nWin + 1
+
+        curVal = grayImg[i, j]
+        gvals[nWin] = curVal
+        c_var = np.mean((gvals[:nWin + 1] - np.mean(gvals[:nWin + 1])) ** 2)
+
+        csig = c_var * 0.6
+        mgv = np.min((gvals[:nWin] - curVal) ** 2)
+        if csig < -mgv / np.log(0.01):
+            csig = -mgv / np.log(0.01)
+
+        if csig < 2e-06:
+            csig = 2e-06
+
+        gvals[:nWin] = np.exp(-(gvals[:nWin] - curVal) ** 2 / csig)
+        gvals[:nWin] = gvals[:nWin] / sum(gvals[:nWin])
+        for each in -gvals[:nWin]:
+            vals.append(each)
+
+        # Now the self-reference (along the diagonal).
+        rows.append(x)
+        cols.append(x)
+        vals.append(1)  # sum(gvals(1:nWin))
+        return x
+
+    # trans = np.vectorize(func, excluded=['indsM_copy', 'grayImg'])
+    # trans(indsM, W, H, winRad, len_window, indsM_copy=indsM_copy, grayImg=grayImg)
+    # trans = np.frompyfunc(func, 1, 1)
+    # trans(indsM)
+    func(2)
+    t2 = time.time()
+    print('t2=%.6'
+          'f' % (t2-t1))
+
+    A = scipy.sparse.csr_matrix((vals, (rows, cols)), (numPix, numPix))
+
+    rows = np.arange(0, numPix)
+    cols = np.arange(0, numPix)
+    vals = (knownValMask * alpha).transpose().reshape(numPix)
+    G = scipy.sparse.csr_matrix((vals, (rows, cols)), (numPix, numPix))
+
+    A = A + G
+    b = np.multiply(vals.reshape(numPix), imgDepth.flatten('F'))
+
+    # print ('Solving system..')
+
+    new_vals = spsolve(A, b)
+    new_vals = np.reshape(new_vals, (H, W), 'F')
+
+    # print ('Done.')
+
+    denoisedDepthImg = new_vals * maxImgAbsDepth
+
+    output = denoisedDepthImg.reshape((H, W)).astype('float32')
+
+    output = np.multiply(output, (1 - knownValMask)) + imgDepthInput
+    t3 = time.time()
+    print('t3=%.2f' % (t3-t2))
+    return output
+
 
 if __name__ == '__main__':
     t1 = time.time()
     rgb_img = np.array(Image.open('0000000016_RGB.png'), dtype=int)
     rgb_img = rgb_img / np.max(rgb_img)
     depth_img = depth_read('0000000016.png')
-    a = fill_depth_colorization_jit(rgb_img, depth_img)
+    a = fill_depth_colorization_fpf(rgb_img, depth_img)
+
     print(a.shape)
     print(np.min(a), np.max(a))
     t2 = time.time()
