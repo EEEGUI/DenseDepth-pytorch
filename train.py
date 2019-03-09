eb021e77c1ea13dd7d7eb1dd2a61f170bf05255c
lin <1462243304@qq.com>
2019-03-09 15:47:37 +0800

    update code

--- train.py
+++ train.py
@@ -3,13 +3,15 @@ from loss import criterion
 import torch.optim as optim
 import torch
 from Dataloader import dataset_loader
-from utils import scale_up
-import torch.nn as nn
+from tensorboardX import SummaryWriter
+
 
 def train():
+    writer = SummaryWriter('log')
     device = torch.device('cuda')
     net = DDNet().to(device)
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    n_batch = len(dataset_loader)
     for epoch in range(10):  # loop over the dataset multiple times
         running_loss = 0.0
         for i, batch_sample in enumerate(dataset_loader):
@@ -26,6 +28,7 @@ def train():
             loss.backward()
             optimizer.step()
 
+            writer.add_scalar('Train/Loss', loss.item(), epoch*n_batch + (i+1))
             # print statistics
             running_loss += loss.item()
             if (i+1) % 1 == 0:  # print every 2000 mini-batches
@@ -34,6 +37,6 @@ def train():
                 running_loss = 0.0
 
     print('Finished Training')
-
+    torch.save(net.state_dict(), 'densedepth.pt')
 if __name__ == '__main__':
     train()
